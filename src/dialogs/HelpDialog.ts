78125d7c8c5967f1e7721cf65aba487d688a7398
Joshua Trick <jotrick@microsoft.com>
2017-08-25 17:20:03 -0700

    Added more helpful help text

--- src/dialogs/HelpDialog.ts
+++ src/dialogs/HelpDialog.ts
@@ -6,7 +6,12 @@ import { DialogMatches } from "../utils/DialogMatches";
 export class HelpDialog extends TriggerActionDialog {
 
     private static async step1(session: builder.Session, args?: any | builder.IDialogResult<any>, next?: (args?: builder.IDialogResult<any>) => void): Promise<void> {
-        session.send("Here is where help would go.");
+        let text = "";
+        text += "'follow tags [tagA, tagB, ...] - Used to start following tags in this conversation<br>";
+        text += "'remove tags [tagA, tagB, ...] - Used to stop following tags in this conversation<br>";
+        text += "'settings' - Used to view and setup the settings for this conversation<br>";
+
+        session.send(text);
 
         session.endDialog();
     }
