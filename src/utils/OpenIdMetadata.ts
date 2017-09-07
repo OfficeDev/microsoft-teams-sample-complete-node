9afc331116c47e2949b25a0145e44603a79e8de0
Adrian Solis <aosolis@microsoft.com>
2017-09-07 12:40:57 -0700

    Add license to new files

--- src/utils/OpenIdMetadata.ts
+++ src/utils/OpenIdMetadata.ts
@@ -1,12 +1,3 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
-// Microsoft Bot Framework: http://botframework.com
-//
-// Bot Builder SDK Github:
-// https://github.com/Microsoft/BotBuilder
-//
 // Copyright (c) Microsoft Corporation
 // All rights reserved.
 //
@@ -29,7 +20,6 @@
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
 
 import * as request from "request";
 let getPem = require("rsa-pem-from-mod-exp");
