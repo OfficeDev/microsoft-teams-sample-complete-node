33dbee37e05361fc9b6b7ebc3288d7dfdd56f31a
Joshua Trick <jotrick@microsoft.com>
2017-09-21 21:08:27 -0700

    Changes made to polish the Glitch experience

--- src/pages/ManifestCreatorStart.ts
+++ src/pages/ManifestCreatorStart.ts
@@ -6,12 +6,18 @@ export class ManifestCreatorStart {
     public static getRequestHandler(): express.RequestHandler {
         return async function (req: any, res: any, next: any): Promise<void> {
             let baseUri = config.get("app.baseUri");
+            // a valid base uri cannot simply be your locally running instance
+            let validBaseUri = baseUri && !(/^https:\/\/localhost|^http:\/\/localhost|^localhost/i.test(baseUri));
             let appId = config.get("bot.botId");
+            // this is to check against the default value I put in the env variable for the Glitch deployment
+            let validAppId = appId && appId !== "NeedToSet";
 
             res.render("manifest-creator/manifestCreatorStart", {
                 baseUri: baseUri,
+                validBaseUri: validBaseUri,
                 appId: appId,
-                buttonEnabled: baseUri && appId,
+                validAppId: validAppId,
+                createManifestEnabled: validBaseUri && validAppId,
             });
         };
     }
