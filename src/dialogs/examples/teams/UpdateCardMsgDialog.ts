94016baf15bd8b283028dff8355d6096cbc45425
Amit Agarwal <v-amagra@microsoft.com>
2018-07-05 16:56:54 -0700

    Resolved comments

--- src/dialogs/examples/teams/UpdateCardMsgDialog.ts
+++ src/dialogs/examples/teams/UpdateCardMsgDialog.ts
@@ -3,7 +3,7 @@ import { TriggerActionDialog } from "../../../utils/TriggerActionDialog";
 import { DialogIds } from "../../../utils/DialogIds";
 import { DialogMatches } from "../../../utils/DialogMatches";
 import { Strings } from "../../../locale/locale";
-let config = require("config");
+import * as config from "config";
 
 export class UpdateCardMsgDialog extends TriggerActionDialog {
     // update the card, if user has already setup the card message from below dialog file using update button click
@@ -11,18 +11,16 @@ export class UpdateCardMsgDialog extends TriggerActionDialog {
     private static async updateCardMessage(session: builder.Session, args?: any | builder.IDialogResult<any>, next?: (args?: builder.IDialogResult<any>) => void): Promise<void> {
         if (session.message.replyToId)
         {
-            let buttons = new Array<builder.CardAction>();
             let updateCardCounter = session.message.value.updateCounterKey;
             let messageBackButtonValue = JSON.stringify({ updateCounterKey: ++updateCardCounter });
 
             let messageBackButton = builder.CardAction.messageBack(session, messageBackButtonValue)
                 .displayText(Strings.messageBack_button_display_text)
-                .title(Strings.update_card_button, updateCardCounter)
+                .title(Strings.update_card_button)
                 .text("update card message");
-            buttons.push(messageBackButton);
 
             let newCard = new builder.HeroCard(session)
-                .title(Strings.updated_card_title)
+                .title(Strings.updated_card_title, updateCardCounter)
                 .subtitle(Strings.updated_card_subtitle)
                 .text(Strings.default_text)
                 .images([
@@ -30,12 +28,12 @@ export class UpdateCardMsgDialog extends TriggerActionDialog {
                         .url(config.get("app.baseUri") + "/assets/computer_person.jpg")
                         .alt(session.gettext(Strings.img_default)),
                     ])
-                .buttons(buttons);
+                .buttons([messageBackButton]);
 
-            let newAddress = { ...session.message.address, id: session.message.replyToId };
+            let addressOfMessageToUpdate = { ...session.message.address, id: session.message.replyToId };
 
             let msg = new builder.Message(session)
-                .address(newAddress)
+                .address(addressOfMessageToUpdate)
                 .addAttachment(newCard);
 
             session.connector.update(msg.toMessage(), (err, address) => {
@@ -59,7 +57,7 @@ export class UpdateCardMsgDialog extends TriggerActionDialog {
     ) {
         super(bot,
             DialogIds.UpdateCardMsgDialogId,
-            DialogMatches.UpdateCardMsgDialogMatch,
+            DialogMatches.Update_Card_Msg_Dialog_Intent,
             UpdateCardMsgDialog.updateCardMessage,
         );
     }
--- src/dialogs/examples/teams/UpdateCardMsgSetupDialog.ts
+++ src/dialogs/examples/teams/UpdateCardMsgSetupDialog.ts
@@ -3,36 +3,33 @@ import { TriggerActionDialog } from "../../../utils/TriggerActionDialog";
 import { DialogIds } from "../../../utils/DialogIds";
 import { DialogMatches } from "../../../utils/DialogMatches";
 import { Strings } from "../../../locale/locale";
-let config = require("config");
+import * as config from "config";
 
 export class UpdateCardMsgSetupDialog extends TriggerActionDialog {
     // setup the card message and then user can update the card using below update dialog file
     // microsoft-teams-sample-complete-node\src\dialogs\examples\teams\UpdateCardMsgDialog.ts
     private static async setupCardMessage(session: builder.Session, args?: any | builder.IDialogResult<any>, next?: (args?: builder.IDialogResult<any>) => void): Promise<void> {
-        let buttons = new Array<builder.CardAction>();
         let updateCardCounter = 0;
 
         let messageBackButtonValue = JSON.stringify({ updateCounterKey: ++updateCardCounter });
         let messageBackButton = builder.CardAction.messageBack(session, messageBackButtonValue)
             .displayText(Strings.messageBack_button_display_text)
-            .title(Strings.update_card_button, updateCardCounter)
+            .title(Strings.update_card_button)
             .text("update card message");
 
-        buttons.push(messageBackButton);
-
         let card = new builder.HeroCard(session)
-        .title(Strings.default_title)
-        .subtitle(Strings.default_subtitle)
-        .text(Strings.default_text)
-        .images([
-            new builder.CardImage(session)
-                .url(config.get("app.baseUri") + "/assets/computer_person.jpg")
-                .alt(session.gettext(Strings.img_default)),
-        ])
-        .buttons(buttons);
+            .title(Strings.updated_card_default_title, updateCardCounter)
+            .subtitle(Strings.default_subtitle)
+            .text(Strings.default_text)
+            .images([
+                new builder.CardImage(session)
+                    .url(config.get("app.baseUri") + "/assets/computer_person.jpg")
+                    .alt(session.gettext(Strings.img_default)),
+                ])
+            .buttons([messageBackButton]);
 
         let msg = new builder.Message(session)
-        .addAttachment(card);
+            .addAttachment(card);
 
         session.send(msg);
     }
