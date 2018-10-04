8308ce7b9ab77a7a25442f88812a3d87ab0d39b2
Amit Agarwal <v-amagra@microsoft.com>
2018-10-04 13:35:40 -0700

    Used builder chain list card

--- src/dialogs/examples/basic/ListCardDialog.ts
+++ src/dialogs/examples/basic/ListCardDialog.ts
@@ -4,39 +4,26 @@ import * as config from "config";
 import { TriggerActionDialog } from "../../../utils/TriggerActionDialog";
 import { DialogIds } from "../../../utils/DialogIds";
 import { DialogMatches } from "../../../utils/DialogMatches";
-// import { ListCard, ListCardItem, ListCardItemType } from "../basic/ListCard";
 import { Strings } from "../../../locale/locale";
 
 export class ListCardDialog extends TriggerActionDialog {
 
     private static async step1(session: builder.Session, args?: any | builder.IDialogResult<any>, next?: (args?: builder.IDialogResult<any>) => void): Promise<void> {
-        let list = Array<builderteams.ListCardItem>();
+        let card = new builderteams.ListCard(session)
+                        .title(session.gettext(Strings.list_card_title))
+                        // item1
+                        .addItem(new builderteams.ListCardItem(session)
+                            .icon(config.get("app.baseUri") + "/assets/computer_people.jpg")
+                            .title(session.gettext(Strings.list_card_item1_title))
+                            .subtitle(session.gettext(Strings.list_card_item1_subtitle)))
+                        // item2
+                        .addItem(new builderteams.ListCardItem(session)
+                            .icon(config.get("app.baseUri") + "/assets/computer_person.jpg")
+                            .title(session.gettext(Strings.list_card_item2_title))
+                            .subtitle(session.gettext(Strings.list_card_item2_subtitle)));
 
-        // list item1
-        let item1 = new builderteams.ListCardItem();
-        item1.type(builderteams.ListCardItemType.resultItem);
-        item1.icon(config.get("app.baseUri") + "/assets/computer_people.jpg");
-        item1.title(session.gettext(Strings.list_card_item1_title));
-        item1.subtitle(session.gettext(Strings.list_card_item1_subtitle));
-
-        // list item2
-        let item2 = new builderteams.ListCardItem();
-        item2.type(builderteams.ListCardItemType.resultItem);
-        item2.icon(config.get("app.baseUri") + "/assets/computer_person.jpg");
-        item2.title(session.gettext(Strings.list_card_item2_title));
-        item2.subtitle(session.gettext(Strings.list_card_item1_subtitle));
-
-        // we have added only two items for this sample, list card support multiple items as needed
-        list.push(item1);
-        list.push(item2);
-
-        let card = new builderteams.ListCard();
-        card.title(session.gettext(Strings.list_card_title));
-        card.items(list);
-
-        let message = new builder.Message(session);
-        message.addAttachment(card.toAttachment());
-        session.send(message);
+        session.send(new builder.Message(session)
+                        .addAttachment(card));
         session.endDialog();
     }
 
